b7537b7232426146178e28269cd0846fef1bf4a8
Gitam <gitam.shimoni@gmail.com>
2024-02-19 17:45:59 +0200

    removing unwanted data

--- Controllers/make_call.js
+++ Controllers/make_call.js
@@ -1,7 +1,5 @@
 // const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const accountSid = "AC4fc029c86a2859b0ecc35fbf4225dac0";
 // const authToken = process.env.TWILIO_AUTH_TOKEN;
-const authToken = "512506e3c52fc8b716bc7489e1c989d9";
 const client = require("twilio")(accountSid, authToken);
 
 client.validationRequests
